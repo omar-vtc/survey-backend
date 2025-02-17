f7fe8e1165c43f12396e007d29dc4cecc0eabcb3
omar-vtc <72626558+omar-vtc@users.noreply.github.com>
2025-02-17 17:38:39 +0200

    call

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 const User = mongoose.model("User", UserSchema);
 
 // 📌 POST route to save user data
-router.post("/submit", async (req, res) => {
+router.post("/call", async (req, res) => {
   console.log("Request received at /submit:", req.body);
   try {
     const userData = req.body;
