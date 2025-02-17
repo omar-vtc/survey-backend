87b48624880274360dcd02ced2e9137e150ad6c9
omar-vtc <72626558+omar-vtc@users.noreply.github.com>
2025-02-17 16:45:51 +0200

    port number

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ app.post("/submit", async (req, res) => {
 });
 
 // 📌 Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () =>
   console.log(`🚀 Server running on http://localhost:${PORT}`)
 );
