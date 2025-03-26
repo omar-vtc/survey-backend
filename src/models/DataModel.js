91ec00532c4a7dea016197fa520b2cdd57bf39fe
Omar Khaled <72626558+omar-vtc@users.noreply.github.com>
2025-03-26 05:25:26 +0200

    backend added and all working

---
+++ src/models/DataModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const AnswerSchema = new mongoose.Schema({
+  A: { type: [Number], required: true },
+  B: { type: [Number], required: true },
+  C: { type: [Number], required: true },
+});
+
+const ScoreSchema = new mongoose.Schema({
+  A: { type: Number, required: true },
+  B: { type: Number, required: true },
+  C: { type: Number, required: true },
+});
+
+const DataSchema = new mongoose.Schema({
+  phone: { type: String, required: true, unique: true },
+  answers: { type: AnswerSchema, required: true },
+  scores: { type: ScoreSchema, required: true },
+});
+
+const DataModel = mongoose.model("Machlan", DataSchema);
+
+module.exports = DataModel;
---
+++ src/routes/machlanRoutes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const DataModel = require("../models/DataModel");
+
+const router = express.Router();
+
+router.post("/machlan", async (req, res) => {
+  try {
+    const { phone, answers, scores } = req.body;
+
+    console.log("phone --> ", phone);
+    console.log("answers: -> ", answers);
+    console.log(("scores:--> ", scores));
+
+    if (!phone || !answers || !scores) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const existingData = await DataModel.findOne({ phone });
+    if (existingData) {
+      existingData.answers = answers;
+      existingData.scores = scores;
+      await existingData.save();
+      return res.json({
+        message: "Data updated successfully",
+        data: existingData,
+      });
+    }
+
+    const newUser = new DataModel({ phone, answers, scores });
+    await newUser.save();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+module.exports = router;
