066d251da1fe652021788507a5db6254888041d8
Omar Khaled <72626558+omar-vtc@users.noreply.github.com>
2025-09-15 16:24:28 +0300

    Add TeamWork model and controller
    Introduces TeamWorkSchema for storing phone, answers, and scores. Adds controller functions to add and retrieve team work answers by phone number, handling both creation and update logic.

---
+++ src/controllers/teamWorkController.js
@@ -0,0 +1,58 @@
+const TeamWorkModel = require("../models/TeamWorkSchema");
+
+exports.addMachlanAns = async (req, res) => {
+  try {
+    const { phone, answers, scores } = req.body;
+
+    if (!phone || !answers || !scores) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const existingData = await DataModel.findOne({ phone });
+    if (existingData) {
+      existingData.answers = answers;
+      existingData.scores = scores;
+      await existingData.save();
+      return res.json({
+        message: "Data updated successfully",
+        data: existingData,
+      });
+    }
+
+    const newUser = new DataModel({ phone, answers, scores });
+    await newUser.save();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.getMachlanAns = async (req, res) => {
+  try {
+    const { phone } = req.params;
+
+    if (!phone) {
+      return res.status(400).json({ message: "Phone number is required" });
+    }
+
+    // Find user by phone number
+    const userData = await DataModel.findOne({ phone });
+
+    if (!userData) {
+      return res.json({ message: "No data found", data: {} });
+    }
+
+    const userWithUpdatedData = {
+      ...userData.toObject(), // Convert Mongoose document to plain object
+      name: "Team Work", // Replace with dynamic name if available
+    };
+
+    res.json({
+      message: "Data retrieved successfully",
+      data: userWithUpdatedData,
+    });
+  } catch (error) {
+    console.error("❌ Error fetching data:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
