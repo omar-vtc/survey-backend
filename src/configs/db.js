7589ed946c18c08656f0237190edc6db8bdc9cce
Omar Khaled <72626558+omar-vtc@users.noreply.github.com>
2025-09-15 03:02:54 +0300

    Update project structure and db config
    Moved .env file to project root for standardization. Updated package.json to use index.js as main entry and changed start script to use src/server.js. Improved db.js by ensuring dotenv is loaded before using environment variables and added a debug log for MONGO_URI.

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,9 +1,9 @@
+require("dotenv").config(); // Load environment variables from .env
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const mongoose = require("mongoose");
 
-require("dotenv").config(); // Load environment variables from .env
-
 const uri = process.env.MONGO_URI; // Use environment variable for security
+console.log(process.env.MONGO_URI);
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
